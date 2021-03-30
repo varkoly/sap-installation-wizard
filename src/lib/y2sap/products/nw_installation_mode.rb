73074d3e341b1c64cb5ca584a09df91398ca1985
Peter Varkoly <varkoly@suse.com>
2021-03-30 16:06:56 +0200

    Adapt suggestions of rubocop

--- src/lib/y2sap/products/nw_installation_mode.rb
+++ src/lib/y2sap/products/nw_installation_mode.rb
@@ -1,5 +1,4 @@
 # encoding: utf-8
-  
 # Copyright (c) [2018] SUSE LLC
 #
 # All Rights Reserved.
@@ -29,9 +28,9 @@ module Y2Sap
     include Yast
     include Yast::UI
     include Yast::UIShortcuts
-    def select_nw_installation_mode()
+    def select_nw_installation_mode
       textdomain "sap-installation-wizard"
-      log.info("-- Start select_nw_installation_mode --- for instDir #{@media.inst_dir}" )
+      log.info("-- Start select_nw_installation_mode --- for instDir #{@media.inst_dir}")
       create_content_nw_installation_mode
       do_loop_nw_installation_mode
     end
@@ -40,51 +39,54 @@ module Y2Sap
 
     # Creates the gui
     def create_content_nw_installation_mode
-      log.info("-- Start SelectNWInstallationMode --- for instDir #{@media.inst_dir}" )
+      log.info("-- Start SelectNWInstallationMode --- for instDir #{@media.inst_dir}")
 
       # Reset the the selected product specific parameter
-      @product_map    = @media.get_products_for_media(@media.inst_dir )
+      @product_map  = @media.get_products_for_media(@media.inst_dir)
+      @inst_type    = ""
+      @db           = ""
+      @product_id   = ""
+      @product_name = ""
       log.info("@product_map #{@product_map}")
-      @inst_type     = ""
-      @db            = ""
-      @product_id    = ""
-      @product_name  = ""
-
-      log.info("known variables " + self.instance_variables.join(" ") )
-      log.info("@dialog_text #{@dialog_text}")
       Wizard.SetContents(
         @dialog_text[:nw_inst_type][:name],
         VBox(
-          HVSquash(Frame("",
-          VBox(
-            HBox(
-                VBox(
+          HVSquash(
+            Frame("",
+              VBox(
+                HBox(
+                  VBox(
                     Left(Label(_("Installation Type"))),
-                    RadioButtonGroup( Id(:type),
-                    VBox(
-                        RadioButton( Id("STANDARD"),    Opt(:notify, :hstretch), _("SAP Standard System"), false),
-                        RadioButton( Id("DISTRIBUTED"), Opt(:notify, :hstretch), _("Distributed System"), false),
-                        # RadioButton( Id("SUSE-HA-ST"),  Opt(:notify, :hstretch), _("SUSE HA for SAP Simple Stack"), false),
-                        RadioButton( Id("HA"),          Opt(:notify, :hstretch), _("SAP High-Availability System"), false),
-                        RadioButton( Id("STANDALONE"),  Opt(:notify, :hstretch), _("SAP Standalone Engines"), false),
-                        RadioButton( Id("SBC"),         Opt(:notify, :hstretch), _("System Rename"), false),
-                    )),
-                ),
-                HSpacing(3),
-                VBox(
+                    RadioButtonGroup(
+                      Id(:type),
+                      VBox(
+                        RadioButton(Id("STANDARD"),    Opt(:notify, :hstretch), _("SAP Standard System"), false),
+                        RadioButton(Id("DISTRIBUTED"), Opt(:notify, :hstretch), _("Distributed System"), false),
+                        RadioButton(Id("HA"),          Opt(:notify, :hstretch), _("SAP High-Availability System"), false),
+                        RadioButton(Id("STANDALONE"),  Opt(:notify, :hstretch), _("SAP Standalone Engines"), false),
+                        RadioButton(Id("SBC"),         Opt(:notify, :hstretch), _("System Rename"), false),
+                      )
+                    ),
+                  ),
+                  HSpacing(3),
+                  VBox(
                     Left(Label(_("Back-end Databases"))),
-                    RadioButtonGroup( Id(:db),
-                    VBox(
-                        RadioButton( Id("ADA"),    Opt(:notify, :hstretch), _("SAP MaxDB"), false),
-                        RadioButton( Id("HDB"),    Opt(:notify, :hstretch), _("SAP HANA"), false),
-                        RadioButton( Id("SYB"),    Opt(:notify, :hstretch), _("SAP ASE"), false),
-                        RadioButton( Id("DB6"),    Opt(:notify, :hstretch), _("IBM DB2"), false),
-                        RadioButton( Id("ORA"),    Opt(:notify, :hstretch), _("Oracle"), false)
-                    ))
+                    RadioButtonGroup(
+                      Id(:db),
+                      VBox(
+                        RadioButton(Id("ADA"),    Opt(:notify, :hstretch), _("SAP MaxDB"), false),
+                        RadioButton(Id("HDB"),    Opt(:notify, :hstretch), _("SAP HANA"), false),
+                        RadioButton(Id("SYB"),    Opt(:notify, :hstretch), _("SAP ASE"), false),
+                        RadioButton(Id("DB6"),    Opt(:notify, :hstretch), _("IBM DB2"), false),
+                        RadioButton(Id("ORA"),    Opt(:notify, :hstretch), _("Oracle"), false)
+                      )
+                    )
+                  )
                 )
-            ),
+              )
+            )
           )
-        ))),
+        ),
         @dialog_text[:nw_inst_type][:help],
         true,
         true
@@ -100,8 +102,6 @@ module Y2Sap
          UI.ChangeWidget(Id("STANDARD"),    :Enabled, false)
          UI.ChangeWidget(Id("DISTRIBUTED"), :Enabled, false)
          UI.ChangeWidget(Id("HA"),          :Enabled, false)
-         # Does not exists at the time
-         # UI.ChangeWidget(Id("SUSE-HA-ST"),  :Enabled, false)
          UI.ChangeWidget(Id("ADA"), :Enabled, false)
          UI.ChangeWidget(Id("HDB"), :Enabled, false)
          UI.ChangeWidget(Id("SYB"), :Enabled, false)
@@ -185,7 +185,6 @@ module Y2Sap
            UI.ChangeWidget(Id("ORA"), :Enabled, false)
            @db = data_base
          when "ORA"
-           # FATE
            Yast::Popup.Error( _("The Installation of Oracle Databas with SAP Installation Wizard is not supported."))
            return :abort
          end
